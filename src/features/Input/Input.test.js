bbeececb0e621e42ea1de9d8d41056d677991e3a
Hayoun Lee <sidhlee@gmail.com>
2020-01-10 20:55:43 +0900

    Add tests on Input component

--- src/features/Input/Input.test.js
+++ src/features/Input/Input.test.js
@@ -8,24 +8,55 @@ import InputContainer from "./InputContainer";
 
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<InputContainer store={store} />);
-  console.log(wrapper.debug());
+  const wrapper = shallow(<InputContainer store={store} />)
+    .dive() // get past HOC <ContextProvider />
+    .dive(); // get past Input to the returned JSX element <div>
   return wrapper;
 };
 
-setup();
-
 describe("render", () => {
   describe("word has not been guessed", () => {
-    test("renders component without error", () => {});
-    test("renders the input control", () => {});
-    test("renders a submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = {
+        success: false
+      };
+      wrapper = setup(initialState);
+    });
+    test("renders component without error", () => {
+      const components = findByTestAttr(wrapper, "component-input");
+      expect(components.length).toBe(1);
+    });
+    test("renders the input control", () => {
+      const inputControls = findByTestAttr(wrapper, "input-control");
+      expect(inputControls.length).toBe(1);
+    });
+    test("renders a submit button", () => {
+      const submitButtons = findByTestAttr(wrapper, "submit-button");
+      expect(submitButtons.length).toBe(1);
+    });
   });
 
   describe("word has been guessed successfully", () => {
-    test("renders component without error", () => {});
-    test("does not render the input control", () => {});
-    test("does not render a submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = {
+        success: true
+      };
+      wrapper = setup(initialState);
+    });
+    test("renders component without error", () => {
+      const components = findByTestAttr(wrapper, "component-input");
+      expect(components.length).toBe(1);
+    });
+    test("does not render the input control", () => {
+      const inputControls = findByTestAttr(wrapper, "input-control");
+      expect(inputControls.length).toBe(0);
+    });
+    test("does not render a submit button", () => {
+      const submitButtons = findByTestAttr(wrapper, "submit-button");
+      expect(submitButtons.length).toBe(0);
+    });
   });
 
   describe("update state", () => {});
