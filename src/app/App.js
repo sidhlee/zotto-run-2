835b0ab21992ba2b0f49db3fdb352c1e96273a8b
Hayoun Lee <sidhlee@gmail.com>
2020-01-10 17:34:07 +0900

    Add child components to App

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,32 @@
 import React from "react";
 import logo from "../logo.svg";
+import Congrats from "../features/Congrats/Congrats";
+import GuessedWords from "../features/GuessedWords/GuessedWords";
+import { getLetterMatchCount } from "../common/utils/";
 
 function App() {
-  return <div className="App">Body</div>;
+  const secretWord = "party";
+  const guessedWords = [
+    {
+      guessedWord: "train",
+      letterMatchCount: getLetterMatchCount("train", secretWord)
+    },
+    {
+      guessedWord: "agile",
+      letterMatchCount: getLetterMatchCount("agile", secretWord)
+    },
+    {
+      guessedWord: "train",
+      letterMatchCount: getLetterMatchCount("train", secretWord)
+    }
+  ];
+  return (
+    <div className="App">
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords guessedWords={guessedWords} />
+    </div>
+  );
 }
 
 export default App;
