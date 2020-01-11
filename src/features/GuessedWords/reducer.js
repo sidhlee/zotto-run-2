f16b7ddc507fb7106bcf18d232b632f5c39fc5ee
Hayoun Lee <sidhlee@gmail.com>
2020-01-11 15:31:01 +0900

    Add bp for guessWord action dispatcher + guessedWordsReducer

---
+++ src/features/GuessedWords/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {};
+/**
+ * @function guessedWordsReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - actions to be reduced.
+ * @returns {array} - New guessed words state.
+ */
+const guessedWordsReducer = (state = initialState, action) => {
+  return null;
+};
+
+export default guessedWordsReducer;
--- src/features/Input/actions.js
+++ src/features/Input/actions.js
@@ -1,1 +1,13 @@
 export const CORRECT_GUESS = "CORRECT_GUESS";
+export const GUESS_WORD = "GUESS_WORD";
+
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ * and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param {string} guessedWord - Guessed words.
+ * @returns {function} - Redux thunk function.
+ */
+export const guessWord = guessedWord => {
+  return (dispatch, getState) => {};
+};
